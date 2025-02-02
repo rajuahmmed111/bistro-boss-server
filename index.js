76d8d0c54fc9d198ca98fd3aea93d9f1f78fa9e8
Raju Ahmmed <ahraju2003@gmail.com>
2025-02-02 13:04:38 +0600

    user delete api

--- index.js
+++ index.js
@@ -48,6 +48,12 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await userCollection.deleteOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
+
     // carts collection
     app.post("/carts", async (req, res) => {
       const cartItem = req.body;
