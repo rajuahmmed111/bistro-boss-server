4d21ed402141e7ec79a773e15283115e13b5c8a3
Raju Ahmmed <ahraju2003@gmail.com>
2025-02-02 15:36:22 +0600

    add admin route

--- index.js
+++ index.js
@@ -54,6 +54,18 @@ async function run() {
       res.send(result);
     });
 
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
     // carts collection
     app.post("/carts", async (req, res) => {
       const cartItem = req.body;
